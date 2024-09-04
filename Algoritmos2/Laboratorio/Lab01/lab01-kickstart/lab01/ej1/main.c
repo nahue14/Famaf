85d4a039314d9b147e6344eb3cb93ecc8570e4c0
Nahuel Ezequiel Leal <nahuel.leal@mi.unc.edu.ar>
2024-09-04 20:44:25 -0300

    traducien y comprension de la consigna

--- Algoritmos2/Laboratorio/Lab01/lab01-kickstart/lab01/ej1/main.c
+++ Algoritmos2/Laboratorio/Lab01/lab01-kickstart/lab01/ej1/main.c
@@ -1,14 +1,20 @@
 /* First, the standard lib includes, alphabetically ordered */
+/* Primero, incluimos las librerias estandard ordenadas alfabéticamente */
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* Maximum allowed length of the array */
+/* Longitud máxima permitida del arreglo */
+
 #define MAX_SIZE 100000
 
-void print_help(char *program_name) {
+void print_help(char *program_name){
+
     /* Print the usage help of this program. */
+    /* Imprime la ayuda para usar este programa. */
+
     printf("Usage: %s <input file path>\n\n"
            "Loads an array given in a file in disk and prints it on the screen."
            "\n\n"
@@ -24,14 +30,21 @@ void print_help(char *program_name) {
            program_name);
 }
 
-char *parse_filepath(int argc, char *argv[]) {
+char *parse_filepath(int argc, char *argv[]){
+    
     /* Parse the filepath given by command line argument. */
+    /* Imprime la ayuda para usar este programa. */
+
     char *result = NULL;
+
     // Program takes exactly two arguments
+    // El programa toma exactamente dos argumentos
+    
     // (the program's name itself and the input-filepath)
+    // (el nombre del programa en sí y la ruta del archivo de entrada)
     bool valid_args_count = (argc == 2);
 
-    if (!valid_args_count) {
+    if (!valid_args_count){
         print_help(argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -41,30 +54,44 @@ char *parse_filepath(int argc, char *argv[]) {
     return result;
 }
 
-unsigned int array_from_file(int array[],
-                             unsigned int max_size,
-                             const char *filepath) {
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath){
     //your code here!!!
 }
 
-void array_dump(int a[], unsigned int length) {
+void array_dump(int a[], unsigned int length){
     //your code here!!!
 }
 
 
-int main(int argc, char *argv[]) {
-    char *filepath = NULL;
+int main(int argc, char *argv[]){
+    
+    /*argc: es la cantidad de argumentos que va a tener el programa, el argumento 0 es 
+     * el nombre del programa ./"nombre del programa" lo que ponga despues del nombre del
+     * programa van a ser los argumentos siguientes
+     * argv se va a guardar la cadena de strings en la posicion correspondiente*/
+
+    char *filepath = NULL; /* Se inicializa una variable puntero nula osea no apunta
+    a ninguna direccion de memoria */
 
     /* parse the filepath given in command line arguments */
+    /* analiza la ruta del archivo proporcionada en los argumentos de la línea de comando */
+
+    /* Guarda en una variable de tipo puntero, los argumentos sin el nombre del programa */
     filepath = parse_filepath(argc, argv);
     
     /* create an array of MAX_SIZE elements */
+    /* crea un arreglo de tamano MAX_SIZE */
     int array[MAX_SIZE];
     
     /* parse the file to fill the array and obtain the actual length */
+    /* analiza el archivo para llenar el arreglo y obtener la longitud real */
+    
+    /* una variable de tipo int no signada osea que solo va a tener numeros positivos */
+
     unsigned int length = array_from_file(array, MAX_SIZE, filepath);
     
-    /*dumping the array*/
+    /* dumping the array */
+    /* volcando el arreglo */
     array_dump(array, length);
     
     return EXIT_SUCCESS;
