dc829a9bceb0512e62b93b36b02a7da22f679199
Nahuel Ezequiel Leal <nahuel.leal@mi.unc.edu.ar>
2024-09-04 22:59:35 -0300

    Correccion de errores x6 guardar la variable que va a retornar en la funcion array_from_file de tipo puntero a int

--- Algoritmos2/Laboratorio/Lab01/lab01-kickstart/lab01/ej1/main.c
+++ Algoritmos2/Laboratorio/Lab01/lab01-kickstart/lab01/ej1/main.c
@@ -70,7 +70,7 @@ char *parse_filepath(int argc, char *argv[]){
 unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath){
     /*usar man pages y fopen, fscanf, fclose*/
     
-    unsigned int vararray = array;
+    unsigned int *vararray = array;
 
     FILE *file = fopen(filepath, "r");
 
